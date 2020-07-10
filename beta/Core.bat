a08f784a0f1eff97466edb2c5bf18b4aada04c62
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-07-10 17:51:42 +0800

    Add files via upload

--- beta/Core.bat
+++ beta/Core.bat
@@ -50,6 +50,7 @@ echo 请重新选择!
 ping 127.1 /n 2 >nul
 goto menu
 :update
+attrib +s +r Core.bat&attrib +s +r sha256.lib
 if "%address%"=="no" echo 不可更新!&pause&goto menu
 echo 检查更新中……
 certutil -urlcache * delete >nul
@@ -115,6 +116,7 @@ copy %0 Core.bat
 del %0&Core.bat
 goto exit
 :forceupdate
+attrib +s +r Core.bat&attrib +s +r sha256.lib
 cls&echo 取消只读......
 attrib -s -r Core.bat&attrib -s -r sha256.lib
 cls&echo 下载sha256.lib中......
@@ -125,4 +127,5 @@ cls&echo 继续......
 start "" updatecore.bat updated-1&exit
 goto:eof
 :exit
+attrib +s +r Core.bat&attrib +s +r sha256.lib
 exit
